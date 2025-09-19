5152be0e19b9942c449ed24578ecf389d32209e9
LuisAR219 <luis.alzate3@udea.edu.co>
2025-09-19 18:01:44 -0500

    Creación función encriptarArreglo

--- Def1_20252/main.cpp
+++ Def1_20252/main.cpp
@@ -17,3 +17,10 @@ unsigned char rotarIzquierda(unsigned char valor, int n) {
 unsigned char rotarDerecha(unsigned char valor, int n) {
     return (unsigned char)((valor >> n) | (valor << (8 - n)));
 }
+
+void encriptarArreglo(unsigned char* entrada, unsigned char* salida, int tam, int n, unsigned char K) {
+    for (int i = 0; i < tam; i++) {
+        unsigned char temp = rotarIzquierda(entrada[i], n);
+        salida[i] = temp ^ K;
+    }
+}
