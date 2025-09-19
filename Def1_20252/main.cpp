61f02b2dd9232ed39d36610f54c9774e1006a9d6
LuisAR219 <luis.alzate3@udea.edu.co>
2025-09-19 13:50:48 -0500

    Creación de funciones para rotar bits

--- Def1_20252/main.cpp
+++ Def1_20252/main.cpp
@@ -2,7 +2,18 @@
 
 using namespace std;
 
+unsigned char rotarIzquierda(unsigned char valor, int n);
+unsigned char rotarDerecha(unsigned char valor, int n);
+
 int main()
 {
 
+}
+
+
+unsigned char rotarIzquierda(unsigned char valor, int n) {
+    return (unsigned char)((valor << n) | (valor >> (8 - n)));
+}
+unsigned char rotarDerecha(unsigned char valor, int n) {
+    return (unsigned char)((valor >> n) | (valor << (8 - n)));
 }
